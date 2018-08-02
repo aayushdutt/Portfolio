7484b7806d53e91a004606cb12139149987a756c
Aayush Dutt <aayushd100@gmail.com>
2018-08-02 23:05:37 +0530

    fix images on mobiles

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import mobile from 'is-mobile'
 
 export default class Landing extends Component {
+  mainImage = mobile() ? <img alt="Aayush Dutt" className="mainImage" src="static/img/mainImage/6.jpg"/>
+                        : <img alt="Aayush Dutt" className="mainImage" src="static/img/mainImage/5.jpg"/> 
   render() {
+    console.log(mobile())
     return (
         <section id="intro" className="section scrollspy full-height">
           <div className="overlay">
-            <img alt="Aayush Dutt" className="mainImage" src="static/img/mainImage/5.jpg"/> 
+            {this.mainImage}
           </div>
           <div className="container">
             <h2>I <span className="teal">Develop</span></h2>
